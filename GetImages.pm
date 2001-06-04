266302908e110987c3f4f3f3a0b9f8ce3497cc09
Lee ♫ Goddard <lgoddard@cpan.org>
2001-06-04 18:07:19 +0000

    Import of LGODDARD/HTTP-GetImages-0.23 from CPAN.
    gitpan-cpan-distribution: HTTP-GetImages
    gitpan-cpan-version:      0.23
    gitpan-cpan-path:         LGODDARD/HTTP-GetImages-0.23.tar.gz
    gitpan-cpan-author:       LGODDARD
    gitpan-cpan-maturity:     released

--- GetImages.pm
+++ GetImages.pm
@@ -1,5 +1,5 @@
 package HTTP::GetImages;
-our $VERSION=0.21;
+our $VERSION=0.23;
 
 =head1 NAME
 
@@ -11,17 +11,35 @@ Recover and locally store images from the web, including those linked by anchor 
 
 Version 0.2+ also gets images from C<A>nchor elements' and image-map C<AREA> elements' C<HREF>/C<SRC>. attributes.
 
+Version 0.23+ allows a limitation on the image size retreived.
+
 =head1 SYNOPSIS
 
 	use HTTP::GetImages;
 	new HTTP::GetImages (
-		'/images/new',
-		qw( http://www.google.co.uk http://www.google.com/ )
+		'/image/save/dir',
+		['http://www.getthese.com/all','http://get.this/'],
+		['http://www.somewhere/ignorethis.html','http://and.this.html']
 	);
+
+	new HTTP::GetImages (
+		'/image/save/dir',
+		['http://www.getthese.com/all','http://get.this/'],
+		['ALL'],
+		'http://www.getthses.com/all/useTHISasROOT/',
+		$minsize
+	);
+
+	print "\nFailed these URLs:-\n";
+	foreach (keys %{$self->{FAILED}})	{ print "\t$_\n" }
+
+	print "\nIgnored these URLs:-\n";
+	foreach (keys %{$self->{IGNORED}})	{ print "\t$_\n" }
 
 =head1 DEPENDENCIES
 
 	strict;
+	warnings;
 	Carp;
 	LWP::UserAgent;
 	HTTP::Request;
@@ -34,20 +52,22 @@ use HTTP::Request;
 use HTML::TokeParser;
 use Carp;
 use strict;
+use warnings;
 no strict 'refs';
 
 =head1 PACKAGE GLOBAL VARIABLE
 
-=item $chat
+=head2 $CHAT
 
-Set it if you'd like a real-time report to C<STDERR>.
+Set to above zero if you'd like a real-time report to C<STDERR>.
 Defaults to off.
 
 =cut
 
-our $chat = undef;
+our $CHAT = 0;
+$CHAT =0;
 
-=item $EXTENSIONS_RE
+=head2 $EXTENSIONS_RE
 
 A regular expression 'or' list of image extensions to match.
 
@@ -59,11 +79,42 @@ Defaults to C<(jpg|jpeg|bmp|gif|png|xbm|xmp)>.
 
 our $EXTENSIONS_RE = '(jpg|jpeg|bmp|gif|png|xbm|xmp)';
 
+=head2 $NEWNAMES
+
+Set to above zero to save files with new names; defaults to using original names.
+
+=cut
+
+our $NEWNAMES = 0;
+
 =head1 CONSTRUCTOR METHOD new
 
-Besides the class reference, accepts the path to the directory in which to store images (no trailing oblique necessary), the remaining paramters being interpreted as an array of URLs.
+Besides the class reference, accepts:
+
+=item 1.
+
+the path to the directory in which to store images (no trailing oblique necessary);
+
+=item 2.
+
+reference to array of URLs to process;
+
+=item 3.
+
+reference to array of URLs to ignore.
+
+If one of these is C<ALL>, then will ignore all B<HTML> documents not in the referenced array of URLs to process.
+If one of these is C<NONE>, will ignore no documents.
 
 Returns a blessed hash, keys of which are:
+
+=item 4.
+
+The minimum path the URL must contain.
+
+=item 5.
+
+The minimum size an image must be to be saved.
 
 =item DONE
 
@@ -75,79 +126,99 @@ a hash, keys of which are the failed URLs, values being short reasons.
 
 =cut
 
-sub new { my ($class,$dir,@urls) = (shift,shift,@_);
-	warn "$class::new requires a directory in which store the images as its second argument." and return undef if not defined $dir;
-	warn "$class::new requires at least one URL to saerch for images as its third argument." and return undef if not defined @urls;
-	warn "Usage: $class::new (\$dir,\@url) " and return undef if not defined $class;
+sub new { my ($class,$dir,$dodo,$dont,$MINURL,$MINIMGSIZE) = (shift,shift,shift,shift,shift,shift);
+	warn "Usage: \$class::new (\$dir,\\\@do,\\\@dont) " and return undef if not defined $class;
+	warn "$class::new requires a directory in which store the images as its first argument." and return undef if not defined $dir;
+	warn "$class::new requires at least one URL to saerch for images as its seoncd argument." and return undef if not defined $dodo;
+	warn "Usage: \$class::new (\$dir,\@do,\@dont) " and return undef if ref $dodo ne 'ARRAY' or (defined $dont and ref $dont ne 'ARRAY');
 
 	my $self={};
 	$self->{DONE} = {};
 	$self->{FAILED} = {};
 	bless $self,$class;
 
-	PAGE:
-	foreach my $page_url (@urls){
-		if (exists $self->{FAILED}->{$page_url} or exists $self->{DONE}->{$page_url}){
-			warn "Already done.\n" if defined $chat;
-			next PAGE;
+	$self->{MINIMGSIZE} = $MINIMGSIZE if defined $MINIMGSIZE;
+	warn "Minimum image size defined as $self->{MINIMGSIZE}.\n" if $CHAT;
+
+	$self->{MINURL} = $MINURL if defined $MINURL;
+	warn "URL must being with $MINURL ..." if $CHAT;
+
+	my @urls;
+
+	foreach (@$dodo){ push @urls,$_; $self->{DO}->{$_}=1; }
+	undef $dodo;
+	foreach (@$dont){ $self->{IGNORE}->{$_} = '1' }
+	undef $dont;
+
+
+	DOC:
+	while (my $doc_url = shift @urls){
+		warn "-"x60,"\n" if $CHAT;
+		my ($doc,$p);
+
+		if (exists $self->{MINURL} and $doc_url !~ /^$self->{MINURL}/){
+			warn "URL out of scope, ignoring $doc_url\n" if $CHAT;
+			next DOC;
 		}
-		warn "*** Parsing $page_url\t\t\t\t"  if defined $chat;
 
-		my $doc = &get_document($page_url)
-			or warn "* Couldn't create parser"
-			and ($self->{FAILED}->{$page_url} = "Agent couldn't open page")
-			and next PAGE;
+		if (exists $self->{FAILED}->{$doc_url} or exists $self->{DONE}->{$doc_url}){
+			warn "Already done $doc_url.\n" if $CHAT;
+			next DOC;
+		}
 
-		my $p = new HTML::TokeParser( \$doc )
-			or warn "* Couldn't create parser"
-			and ($self->{FAILED}->{$page_url} = "Couldn't create agent parser")
-			and next PAGE;
+		if (exists $self->{IGNORE}->{$doc_url}){
+			warn "In IGNORE list: $doc_url.\n" if $CHAT;
+			next DOC;
+		}
 
-		TOKE:
+		# Not in do list, not an image, not run with IGNORE NONE option
+		if (not exists $self->{DO}->{$doc_url} and $doc_url !~ m|(\.$EXTENSIONS_RE)$|i
+		and not exists $self->{IGNORE}->{NONE}){
+			warn "Not in DO list - ignoreing $doc_url .\n" if $CHAT;
+			$self->{IGNORE}->{$doc_url} = "Ignoring";
+			next DOC;
+		}
+
+		unless ($doc = $self->get_document($doc_url)){
+			$self->{FAILED}->{$doc_url} = "Agent couldn't open page";
+			next DOC;
+		}
+
+		# If an image, save it
+		if ($doc_url =~ m|(\.$EXTENSIONS_RE)$|i) {
+			$self->{DONE}->{$doc_url} = $self->_save_img($dir,$doc_url,$doc);
+			next DOC;
+		} else {
+			$self->{DONE}->{$doc_url} = "Did HTML.";
+		}
+
+		# Otherwise try to parse it
+		unless ($p = new HTML::TokeParser( \$doc )){
+			warn "* Couldn't create parser from \$doc\n" if $CHAT;
+			$self->{FAILED}->{$doc_url} = "Couldn't create agent parser";
+			next DOC;
+		}
+		warn "OK - parsing document $doc_url ...\n" if $CHAT;
+
 		while (my $token = $p->get_token){
-			next TOKE if @$token[1] !~ m/^(img|area|a)$/;
-			next TOKE if @$token[0] ne 'S';	# How does that effect well-formed XHTML?
-
-			my $img_url='';
-
-			# Get an IMG src
 			if (@$token[1] eq 'img'){
-				$img_url = &abs_url( $page_url, @$token[2]->{src} ) ;
-			}
-
-			# Get an AREA href
-			elsif (@$token[1] eq 'area'){
-				$img_url = &abs_url( $page_url, @$token[2]->{href} ) ;
-			}
-
-			# Get an image in an Anchor element
-			elsif (@$token[1] eq 'a' and @$token[0] eq 'S'){
-				my $href = @$token[2]->{href};
-				# Parse up to end of the Anchor element
-				while ($token = $p->get_token and not (@$token[0] eq 'E' and @$token[1] eq 'a')){
-					$img_url = &abs_url( $page_url, @$token[2]->{src} ) if @$token[1] eq 'img';
-					# Act now to process the gained URL as below
-					if ($img_url =~ m/\.html?/i){
-						push @urls,$img_url
-					} elsif ($img_url =~ m|(\.$EXTENSIONS_RE)$|i) {
-						$self->{DONE}->{$img_url} = $self->_save_img( $dir,$img_url,get_document($img_url));
-					}
-				}
-			} # End if
-
-			# If we got a new URI
-			if (not exists $self->{FAILED}->{$page_url} and not exists $self->{DONE}->{$page_url}){
-				# Act now to process the gained URL as above
-				if ($img_url =~ m/\.html?/i){
-					warn "Will remember to parse $img_url later" if defined $chat;
-					push @urls,$img_url
-				} elsif ($img_url =~ m|(\.$EXTENSIONS_RE)$|i) {
-					$self->{DONE}->{$img_url} = $self->_save_img( $dir,$img_url,get_document($img_url));
+				warn "*** Found image: @$token[2]->{src}\n" if $CHAT;
+				my $uri = &abs_url( $doc_url, @$token[2]->{src} );
+				if (not exists $self->{IGNORE0}->{$uri} and not exists $self->{DONE}->{$uri} and not exists $self->{FAILED}->{$uri}){
+					unshift @urls, $uri;
 				}
 			}
+			elsif (@$token[1] eq 'area' or @$token[1] eq 'a' and @$token[0] eq 'S'){
+				warn "*** Found link: @$token[2]->{href}\n" if $CHAT;
+				my $uri = &abs_url( $doc_url, @$token[2]->{href} );
+				if (not exists $self->{IGNORE}->{$uri} and not exists $self->{DONE}->{$uri} and not exists $self->{FAILED}->{$uri}){
+					unshift @urls, $uri;
+				}
+			}
+		}
 
-		} # Next while TOKE
-	} # Next for PAGE
+	} # Next DOC
+
 	return $self;
 } # End sub new
 
@@ -159,20 +230,30 @@ sub new { my ($class,$dir,@urls) = (shift,shift,@_);
 # SUB get_document
 # Accepts a URL, returns the source of the document at the URL
 #
-sub get_document{
-	my $url = shift;									# Recieve as argument the URL to access
+sub get_document { my ($self,$url) = (shift,shift);		# Recieve as argument the URL to access
 	my $ua = LWP::UserAgent->new;						# Create a new UserAgent
-	$ua->agent('Mozilla/25.0 (getPage0.2');				# Give it a type name
-	warn "Attempting to access <$url>...\n"  if defined $chat;
-	my $req = new HTTP::Request('GET', $url);		# Format URL request
-	my $res = $ua->request($req);					# $res is the object UA returned
-	if ($res->is_success()) {							# If successful
-		warn "...ok.\n" if defined $chat;
-		return $res->content;							# $res->content  is the HTML the UA returned from the URL
-	} else {
-		warn"...failed.\n"  if defined $chat;
-		return "";
+	$ua->agent('Mozilla/25.0'.(localtime).' (PERL HTTP::GetImages $VERSION');	# Give it a type name
+	warn "Attempting to access <$url>...\n"  if $CHAT;
+	my $req = new HTTP::Request('GET', $url) 			# Format URL request
+		or warn "...could not GET.\n" and return undef;
+	my $res = $ua->request($req);						# $res is the object UA returned
+	if (not $res->is_success()) {							# If successful
+		warn"...failed.\n"  if $CHAT;
+		return undef
 	}
+	warn "...ok.\n" if $CHAT;
+	# Test size
+	if ($url =~ m|(\.$EXTENSIONS_RE)$|i) {
+		$_ = length ($res->content);
+		warn "Image file size $_ bytes\n" if $CHAT;
+		if (exists $self->{MINIMGSIZE} and  $_ < $self->{MINIMGSIZE}){
+			warn "Image size too small, ignoring.\n" if $CHAT;
+			$self->{IGNORE}->{$url} = "Size $_ bytes is too small.";
+			return undef;
+		}
+	}
+
+	return $res->content;							# $res->content  is the HTML the UA returned from the URL
 }
 
 
@@ -189,19 +270,24 @@ Returns the path the image was saved at.
 
 sub _save_img { my ($self,$dir,$url,$img) = (shift,shift,shift,shift);
 	local *OUT;
+	my $filename;
 	# Remvoe any file path from the $url
 	if (exists $self->{DONE}->{$url} or exists $self->{FAILED}->{$url}){
-		warn "Already got this one ($url), not saving.\n" if defined $chat;
+		warn "Already got this one ($url), not saving.\n" if $CHAT;
 		return undef;
 	}
-	$url =~ m|/[^./]+(\.$EXTENSIONS_RE)$|i;
-	my $ext = $1;
-	my $filename = $dir.'/'.(join'',localtime).$ext;
-	warn "Saving image as $filename...\n"  if defined $chat;
-	open OUT,">$filename" or warn "Couldn't open to save <$filename>!";
-	binmode OUT;
-	print OUT $img;
+	$url =~ m|/([^./]+)(\.$EXTENSIONS_RE)$|i;
+	if ($NEWNAMES){
+		$filename = $dir.'/'.(join'',localtime).$2;
+	} else {
+		$filename = "$dir/$1$2";
+	}
+	warn "Saving image as <$filename>...\n"  if $CHAT;
+	open OUT,">$filename" or warn "Couldn't open to save <$filename>!" and return "Failed to save.";
+		binmode OUT;
+		print OUT $img;
 	close OUT;
+	warn "...ok.\n" if $CHAT;
 	return $filename;
 }
 
@@ -209,7 +295,15 @@ sub _save_img { my ($self,$dir,$url,$img) = (shift,shift,shift,shift);
 #
 # SUB abs_url returns an absolute URL for a $child_url linked from $parent_url
 #
+# DOC http://www.netverifier.com/pin/nicolette/jezfuzchr001.html
+# SRC /pin/nicolette/jezfuzchr001.jpg
+#
 sub abs_url { my ($parent_url,$child_url) = (shift,shift);
+	my $hack;
+	if ($child_url =~ m|^/|) {
+		$parent_url =~ s|^(http://[\w.]+)?/.*$|$1|i;
+		return $parent_url.$child_url;
+	}
 	if ($child_url =~ m|^\.\.\/|i){
 		$parent_url =~ s/\/[^\/]+$//;	# Strip filename
 		while ($child_url=~s/^\.\.\///gs ){
